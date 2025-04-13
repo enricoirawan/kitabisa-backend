2734d3738e90283c1ae382229c825bd922c723e9
irawanenrico <irawanenrico@gmail.com>
2025-04-13 23:09:21 +0700

    add logging

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -63,6 +63,7 @@ export class AuthService {
       secure: true,
       httpOnly: true,
       expires,
+      sameSite: 'none', // karena frontend/backend beda origin
     });
     return { tokenPayload };
   }
