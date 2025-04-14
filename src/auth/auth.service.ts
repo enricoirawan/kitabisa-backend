f4b43c4bdfbf9ccba28c154695caffebb267b5a6
irawanenrico <irawanenrico@gmail.com>
2025-04-14 12:18:07 +0700

    fix auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
       httpOnly: true,
       expires,
       sameSite: 'lax',
-      domain: this.configService.getOrThrow('FRONT_END_URL'),
+      domain: '.ricoenn.com',
       path: '/',
     });
     return { tokenPayload };
